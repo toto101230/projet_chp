99e2a733bb7accc3616300a166d3b2fdc04d9c8f
toto101230 <tpienne51@gmail.com>
2023-11-14 15:08:45 +0100

    Amélioration de openmp

--- projet_base/main.c
+++ projet_base/main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <omp.h>
+
+//int main() {
+//    int id;
+//#pragma omp parallel private(id)
+//    {
+//        id = omp_get_thread_num();
+//        printf("%d: Hello World!\n", id);
+//    }
+//    return 0;
+//}
+
+#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
-#include <time.h>
+
+// Default maximum number of simultaneous process
+int MAX_FILS = 5;
 
 void grep(char *analyse, char *file, int i) {
-    if (analyse == NULL)
-        return;
     FILE *f = fopen(file, "r");
     if (f == NULL) {
         perror("fopen");
@@ -31,6 +46,7 @@ char *readline(FILE *f) {
     ssize_t read;
     if ((read = getline(&line, &len, f)) != -1) {
         line[read - 2] = '\0';
+
         return line;
     }
     return NULL;
@@ -41,34 +57,43 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: '%s' nb_of_processus dictionnary_file shasum_file num_of_process\n", argv[0]),
                 exit(EXIT_FAILURE);
 
-    clock_t c1 = clock();
 
+    char *p;
+    MAX_FILS = strtol(argv[1], &p, 10);
     char *dict_file = argv[2];
     char *shasum_file = argv[3];
 
-    // opening file
+// opening file
     FILE *ds = fopen(shasum_file, "r");
     if (ds == NULL)
         exit(EXIT_FAILURE);
+//    int current_checker_running = 0;
     char *current_password_to_analyse = readline(ds);
 
 
     int id;
-#pragma omp parallel default(none) shared(current_password_to_analyse, dict_file, ds) private(id)
-    {
-        while (current_password_to_analyse != NULL) {
+    while (current_password_to_analyse != NULL) {
+#pragma omp parallel default(none) shared(dict_file, ds) private(current_password_to_analyse, id)
+        {
             id = omp_get_thread_num();
+//#pragma omp critical
+//            {
             current_password_to_analyse = readline(ds);
-//            printf("\n[INFO] Started proc with id %d searching for password %s\n", id, current_password_to_analyse);
-//            if (current_password_to_analyse != NULL) {
-//                printf("%s est analysé par %d\n", current_password_to_analyse, id);
-            grep(current_password_to_analyse, dict_file, id);
 //            }
+//            printf("\n[INFO] Started proc with id %d searching for password %s\n", id, current_password_to_analyse);
+            if (current_password_to_analyse != NULL) {
+//                printf("%s est analysé par %d\n", current_password_to_analyse, id);
+                grep(current_password_to_analyse, dict_file, id);
+            } else {
+                printf("Fin de l'analyse\n");
+                exit(EXIT_SUCCESS);
+            }
         }
     } // end while
-    printf("Fin de l'analyse\n");
-    clock_t c2 = clock();
-    printf("Temps d'execution: %f\n", (double) (c2 - c1) / CLOCKS_PER_SEC);
+#pragma omp barrier
+    {
+        printf("Fin de l'analyse\n");
+    }
     return 0;
 }
 
