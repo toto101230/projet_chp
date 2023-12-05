8e17e8a8e700e97475a2fb3f88d7b8c962b50b9f
user <mylane.nguyen@gmail.com>
2023-12-05 17:21:27 +0100

    remove comments + fix verbose mode + add more precision for time

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+#include <sys/time.h>
 
 int grep(char *analyse, char *file, int i, int verbose) {
     if (analyse == NULL)
@@ -28,7 +30,6 @@ int grep(char *analyse, char *file, int i, int verbose) {
     return 0;
 }
 
-//wraup for readline
 char *readline(FILE *f) {
     char *line = NULL;
 
@@ -42,24 +43,24 @@ char *readline(FILE *f) {
 }
 
 int main(int argc, char *argv[]) {
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
     if (argc < 3) {
         fprintf(stderr, "Usage: '%s' nb_of_processus dictionnary_file shasum_file num_of_process\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    time_t begin = time(NULL);
-
     char *dict_file = argv[1];
     char *shasum_file = argv[2];
     int verbose = 0;
-    if (argc == 5) {
+    if (argc == 4) {
         if (strcmp(argv[3], "-v") == 0) {
             printf("Verbose mode activated\n");
             verbose = 1;
         }
     }
 
-    // opening file
     FILE *ds = fopen(shasum_file, "r");
     if (ds == NULL)
         exit(EXIT_FAILURE);
@@ -73,18 +74,18 @@ int main(int argc, char *argv[]) {
         while (current_password_to_analyse != NULL) {
             id = omp_get_thread_num();
             current_password_to_analyse = readline(ds);
-//            printf("\n[INFO] Started proc with id %d searching for password %s\n", id, current_password_to_analyse);
-//            if (current_password_to_analyse != NULL) {
-//                printf("%s est analysé par %d\n", current_password_to_analyse, id);
             if (grep(current_password_to_analyse, dict_file, id, verbose)==1) {
                 nb++;
             }
-//            }
         }
-    } // end while
+    }
     printf("Fin de l'analyse avec %d mots de passe trouvés\n", nb);
-    time_t end = time(NULL);
-    printf("Temps d'execution: %ld\n", end - begin);
+    gettimeofday(&end, NULL);
+    printf("Temps d'execution en ms: %ld\n", ((end.tv_sec * 1000000 + end.tv_usec)
+                                        - (start.tv_sec * 1000000 + start.tv_usec)));
+    // Temps en secondes avec 6 chiffres après la virgule
+    printf("Temps d'execution en s: %f\n", ((end.tv_sec * 1000000 + end.tv_usec)
+                                           - (start.tv_sec * 1000000 + start.tv_usec)) / 1000000.0);
     return 0;
 }
 
