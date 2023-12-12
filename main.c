690c1b850deac4ef92b9b7e9ddeb9553689f870d
user <mylane.nguyen@gmail.com>
2023-12-12 13:08:28 +0100

    Clean code and add Amdahl Law graph

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/time.h>
+#include <unistd.h>
 
+
+// Fonction qui récupère les lignes d'un fichier et les stocke dans un tableau
 char **read_file(char *filename, int *size_dict) {
     FILE *f = fopen(filename, "r");
     if (f == NULL)
@@ -29,6 +32,7 @@ char **read_file(char *filename, int *size_dict) {
     return dict;
 }
 
+// Fonction de recherche de mots de passe
 int grep(char *shadow, char **dict, int size_dict, int id, int verbose) {
     if (shadow == NULL)
         return 0;
@@ -47,55 +51,54 @@ int grep(char *shadow, char **dict, int size_dict, int id, int verbose) {
 
 int main(int argc, char *argv[]) {
 
-    // Récupérer le nombre de thread depuis la variable d'environnement OMP_NUM_THREADS
+    struct timeval start, end;
+    struct timeval start_while, end_while;
+    char *dict_file = argv[1];
+    char *shasum_file = argv[2];
+    int verbose = 0;
+    char **dict = NULL;
+    int size_dict = 0;
+    char **shadow = NULL;
+    int size_shadow = 0;
+    int id_thread;
+    int nb_password_found = 0;
+
+    // Récupère le nombre de thread depuis la variable d'environnement OMP_NUM_THREADS
     char *nb_thread = getenv("OMP_NUM_THREADS");
     if (nb_thread == NULL) {
         printf("La variable d'environnement OMP_NUM_THREADS n'est pas définie\n");
         exit(EXIT_FAILURE);
     }
 
-    struct timeval start, end;
-    struct timeval start_while, end_while;
     gettimeofday(&start, NULL);
     if (argc < 3) {
         fprintf(stderr, "Usage: '%s' nb_of_processus dictionnary_file shasum_file num_of_process\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    char *dict_file = argv[1];
-    char *shasum_file = argv[2];
-    int verbose = 0;
-    if (argc == 4) {
-        if (strcmp(argv[3], "-v") == 0) {
-            printf("Verbose mode activated\n");
+    if (argc == 5) {
+        if (strcmp(argv[4], "-v") == 0) {
             verbose = 1;
         }
     }
 
-    char **dict = NULL;
-    int size_dict = 0;
+    // Récupération du contenu des fichiers dictionnaire et shadow dans des tableaux
     dict = read_file(dict_file, &size_dict);
-
-    char **shadow = NULL;
-    int size_shadow = 0;
     shadow = read_file(shasum_file, &size_shadow);
-
 
     FILE *results = fopen("exec_time.txt", "a");
     if (results == NULL)
         exit(EXIT_FAILURE);
 
-    int id;
-    int nb = 0;
     gettimeofday(&start_while, NULL);
-    #pragma omp parallel default(none) shared(dict, size_dict, shadow, size_shadow, nb, verbose) private(id)
+    #pragma omp parallel default(none) shared(dict, size_dict, shadow, size_shadow, nb_password_found, verbose) private(id_thread)
     {
         #pragma omp for
         for(int i = 0; i < size_shadow; i++) {
-            id = omp_get_thread_num();
-            if (grep(shadow[i], dict, size_dict, id, verbose)==1) {
+            id_thread = omp_get_thread_num();
+            if (grep(shadow[i], dict, size_dict, id_thread, verbose)==1) {
                 #pragma omp atomic
-                nb++;
+                nb_password_found++;
             }
         }
     }
@@ -103,19 +106,22 @@ int main(int argc, char *argv[]) {
     {
         gettimeofday(&end_while, NULL);
     }
-    
-
-    printf("Fin de l'analyse avec %d mots de passe trouvés\n", nb);
-    float parallel_exec_time = ((end_while.tv_sec * 1000000 + end_while.tv_usec) - (start_while.tv_sec * 1000000 + start_while.tv_usec)) / 1000000.0;
-    printf("Temps d'execution des taches paralleles en s: %f\n", parallel_exec_time);
     gettimeofday(&end, NULL);
-    float total_exec_time = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)) / 1000000.0;
-    printf("Temps d'execution en s: %f\n", total_exec_time);
-    printf("%f\n", total_exec_time);
 
+    float parallel_exec_time = ((end_while.tv_sec * 1000000 + end_while.tv_usec) - (start_while.tv_sec * 1000000 + start_while.tv_usec)) / 1000000.0;
+    float total_exec_time = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)) / 1000000.0;
+
+    if(verbose == 1) {
+        printf("Fin de l'analyse avec %d mots de passe trouvés\n", nb_password_found);
+        printf("Temps d'execution total en s: %f\n", total_exec_time);
+        printf("Temps d'execution des taches paralleles en s: %f\n", parallel_exec_time);
+    }
+
+    // Ecriture des résultats dans le fichier exec_time.txt
     fprintf(results, "%s\t%f\t%f\n", nb_thread, total_exec_time, parallel_exec_time);
     fclose(results);
 
+    // Libération de la mémoire
     for(int i = 0; i < size_dict; i++) {
         free(dict[i]);
     }
---
+++ script_amdahl.py
@@ -0,0 +1,57 @@
+import os
+import matplotlib.pyplot as plt
+
+OMP_NUM_THREADS = 1
+NB_MAX_THREAD = 30
+nb_exec = 30
+
+# Compiler le fichier main.c
+os.system('gcc -g -o main main.c -fopenmp -Wall')
+
+# Créer un fichier exec_time.txt s'il n'existe pas, sinon le vider
+open('exec_time.txt', 'w').close()
+
+while OMP_NUM_THREADS <= NB_MAX_THREAD:
+    # Changer le nombre de threads utilisés par openmp
+    os.environ['OMP_NUM_THREADS'] = str(OMP_NUM_THREADS)
+    for i in range(nb_exec):
+        # Récupérer le temps d'exécution dans le fichier exec_time.txt
+        os.system(f'./main projet_base/dictSortie.txt projet_base/shadow.txt exec_time.txt')
+    OMP_NUM_THREADS += 1
+
+# Ouvrir le fichier exec_time.txt et récupérer les données
+with open('exec_time.txt', 'r') as f:
+    data = f.readlines()
+
+results = {}
+nb = 1
+result = []
+
+for line in data:
+    line = line.split('\t')
+    line[-1] = line[-1].replace('\n', '') # enlever le caractère '\n' à la fin de la ligne
+    nb_threads = int(line[0])
+    total_time = float(line[1])
+    parallel_time = float(line[2])
+    if nb_threads != nb:
+        results[nb] = [sum([result[i][0] for i in range(len(result))]) / len(result),
+                       sum([result[i][1] for i in range(len(result))]) / len(result)]
+        nb = nb_threads
+        result = []
+    result.append([total_time, parallel_time])
+
+results[nb] = [sum([result[i][0] for i in range(len(result))]) / len(result),
+               sum([result[i][1] for i in range(len(result))]) / len(result)]
+
+# Calcul du gain de performance
+def speedup(nb_threads, total_time, parallel_time):
+    return 1 / ((total_time - parallel_time) + (parallel_time / nb_threads))
+
+# Tracer la loi d'Amdahl
+plt.plot(range(1, NB_MAX_THREAD + 1), [speedup(i, results[i][0], results[i][1]) for i in range(1, NB_MAX_THREAD + 1)], label='Total time')
+plt.xlabel('Number of threads')
+plt.ylabel('Speedup (s)')
+plt.title("Amdahl's Law")
+plt.savefig('graph_amdahl.pdf')
+plt.show()
+plt.close()
