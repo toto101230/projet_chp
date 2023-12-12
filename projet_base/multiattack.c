6743757a5abead9f61a8614d2e4a4dc318020124
toto101230 <tpienne51@gmail.com>
2023-12-12 14:49:37 +0100

    graph multiattack.c

--- projet_base/multiattack.c
+++ projet_base/multiattack.c
@@ -8,68 +8,89 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 
 // Default maximum number of simultaneous process
 int MAX_FILS = 5;
 
 //wraup for readline
-char * readline(FILE * f)
-{char * line = NULL;
- 
+char *readline(FILE *f) {
+    char *line = NULL;
+
     size_t len = 0;
-	ssize_t read;
+    ssize_t read;
     if ((read = getline(&line, &len, f)) != -1) {
-	line[read-2] = '\0';
-	
-      return line;
+        line[read - 2] = '\0';
+
+        return line;
     }
-	return NULL;
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
-     if(argc < 2) fprintf(stderr, "Usage: '%s' nb_of_processus dictionnary_file shasum_file num_of_process\n", argv[0]), exit(EXIT_FAILURE);
-    char *p;
-	MAX_FILS = strtol(argv[1], &p, 10);
-	char * dict_file = argv[2];
-	char * shasum_file = argv[3];
+    struct timeval start, end;
+    struct timeval start_while, end_while;
+    gettimeofday(&start, NULL);
+    FILE *results = fopen("exec_time.txt", "a");
+    if (results == NULL)
+        exit(EXIT_FAILURE);
 
-	// opening file
-	FILE * ds = fopen(shasum_file, "r");
+    if (argc < 2)
+        fprintf(stderr, "Usage: '%s' nb_of_processus dictionnary_file shasum_file num_of_process\n", argv[0]), exit(
+                EXIT_FAILURE);
+    char *p;
+    MAX_FILS = strtol(argv[1], &p, 10);
+    char *dict_file = argv[2];
+    char *shasum_file = argv[3];
+
+    // opening file
+    FILE *ds = fopen(shasum_file, "r");
     if (ds == NULL)
         exit(EXIT_FAILURE);
-    char * current_password_to_analyse = readline(ds);
-	int current_checker_running = 0;
+    char *current_password_to_analyse = readline(ds);
+    int current_checker_running = 0;
 
 
+    gettimeofday(&start_while, NULL);
     int n = 0;
-    while (current_password_to_analyse!=NULL)
-	{
-	  for ( ; current_checker_running < MAX_FILS ; )
-	  { // forking until reaching MAX_FILS sons
-		if ((n=fork())<0)
-			{
-				perror("fork error");
-				exit(1);
-			}
-		if (n!=0) {//Father code, so I'm couting one more son
+    while (current_password_to_analyse != NULL) {
+        for (; current_checker_running < MAX_FILS;) { // forking until reaching MAX_FILS sons
+            if ((n = fork()) < 0) {
+                perror("fork error");
+                exit(1);
+            }
+            if (n != 0) {//Father code, so I'm couting one more son
 
-			current_checker_running++;
-			printf("\n[INFO] Started  %dth son searching for password %s\n", current_checker_running, current_password_to_analyse);
-current_password_to_analyse = readline(ds);
+                current_checker_running++;
+                printf("\n[INFO] Started  %dth son searching for password %s\n", current_checker_running,
+                       current_password_to_analyse);
+                current_password_to_analyse = readline(ds);
 
-			  }
-			else { // son code.
-				execl("/bin/grep", "grep", current_password_to_analyse, dict_file ,NULL);
-			       // There's no return from execl, remember it ?
-			  }
+            } else { // son code.
+                execl("/bin/grep", "grep", current_password_to_analyse, dict_file, NULL);
+                // There's no return from execl, remember it ?
+            }
 
-	} // for end. So I should have more or less MAX_FILS sons, until one's returning ...
-	pid_t any_child;
-	wait(&any_child);
-	printf("One of my son has finished\n. I'll start another one if I need to...");
-	// one son has terminated
-	current_checker_running--;
-	} // end while
-return 0;
+        } // for end. So I should have more or less MAX_FILS sons, until one's returning ...
+        pid_t any_child;
+        wait(&any_child);
+        printf("One of my son has finished\n. I'll start another one if I need to...");
+        // one son has terminated
+        current_checker_running--;
+    } // end while
+
+
+    gettimeofday(&end_while, NULL);
+    gettimeofday(&end, NULL);
+    float parallel_exec_time =
+            ((end_while.tv_sec * 1000000 + end_while.tv_usec) - (start_while.tv_sec * 1000000 + start_while.tv_usec)) /
+            1000000.0;
+    float total_exec_time =
+            ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)) / 1000000.0;
+
+    // Ecriture des résultats dans le fichier exec_time.txt
+    fprintf(results, "%d\t%f\t%f\n", MAX_FILS, total_exec_time, parallel_exec_time);
+    fclose(results);
+    return 0;
 }
 
---
+++ script_graph_multi.py
@@ -0,0 +1,57 @@
+import os
+import matplotlib.pyplot as plt
+
+OMP_NUM_THREADS = 1
+NB_MAX_THREAD = 50
+nb_exec = 20
+
+# Compiler le fichier mutliattack. du sous-dossier projet_base
+os.system('cd projet_base && gcc -o multi multiattack.c -Wall && cd ..')
+
+
+# Créer un fichier exec_time.txt s'il n'existe pas, sinon le vider
+open('exec_time.txt', 'w').close()
+
+while OMP_NUM_THREADS <= NB_MAX_THREAD:
+    # Changer le nombre de threads utilisés par openmp
+    # os.environ['OMP_NUM_THREADS'] = str(OMP_NUM_THREADS)
+    for i in range(nb_exec):
+        # Récupérer les temps d'exécution dans le fichier exec_time.txt
+        os.system(f'./projet_base/multi '+str(OMP_NUM_THREADS)+' projet_base/dictSortie.txt projet_base/shadowSmall.txt')
+    OMP_NUM_THREADS += 1
+
+# Récupération des données du fichier exec_time.txt
+with open('exec_time.txt', 'r') as f:
+    data = f.readlines()
+    f.close()
+
+results = {}
+nb = 1
+result = []
+
+for line in data:
+    line = line.split('\t')
+    line[-1] = line[-1].replace('\n', '') # enlever le caractère '\n' à la fin de la ligne
+    nb_threads = int(line[0])
+    total_time = float(line[1])
+    parallel_time = float(line[2])
+    # Calcul de la moyenne des runs
+    if nb_threads != nb:
+        results[nb] = [sum([result[i][0] for i in range(len(result))]) / len(result),
+                       sum([result[i][1] for i in range(len(result))]) / len(result)]
+        nb = nb_threads
+        result = []
+    result.append([total_time, parallel_time])
+
+results[nb] = [sum([result[i][0] for i in range(len(result))]) / len(result),
+               sum([result[i][1] for i in range(len(result))]) / len(result)]
+
+
+# Affichage du graphique du nombre de threads en fonction du temps total d'exécution
+plt.plot(range(1, NB_MAX_THREAD + 1), [results[i][0] for i in range(1, NB_MAX_THREAD + 1)], label='Total time')
+plt.xlabel('Number of threads')
+plt.ylabel('Total Execution Time (s)')
+plt.title('Total Execution Time by Number of Threads')
+plt.savefig('graph_multi.png')
+plt.show()
+plt.close()
